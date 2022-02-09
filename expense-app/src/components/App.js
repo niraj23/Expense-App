80ab57c8162344ef4554afe72f89294628dfa336
Niraj <niraj2323@gmail.com>
2022-02-09 01:46:30 -0500

    Completed

--- expense-app/src/components/App.js
+++ expense-app/src/components/App.js
@@ -1,24 +1,25 @@
-import '../assets/App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { clearNotifications } from '../actions';
-import { authenticateUser } from '../slice/user';
-import Home from './Home';
-import Main from './Main';
-import SignUp from '../containers/SignUp';
-import LogIn from '../containers/LogIn';
-import Dev from '../containers/Dev';
-import alanBtn from '@alan-ai/alan-sdk-web';
+import "../assets/App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { clearNotifications } from "../actions";
+import { authenticateUser } from "../slice/user";
+import Home from "./Home";
+import Main from "./Main";
+import SignUp from "../containers/SignUp";
+import LogIn from "../containers/LogIn";
+// import Dev from '../containers/Dev';
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-const alanKey = "ec782c9839e4d50b2bf11f1ffa85037b2e956eca572e1d8b807a3e2338fdd0dc/stage";
+const alanKey =
+  "ec782c9839e4d50b2bf11f1ffa85037b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   const dispatch = useDispatch();
   const resetAlerts = () => dispatch(clearNotifications());
 
   useEffect(() => {
-    const storage = localStorage.getItem('exp_tracker');
+    const storage = localStorage.getItem("exp_tracker");
     if (storage) {
       dispatch(authenticateUser(JSON.parse(storage)));
     }
@@ -35,7 +36,6 @@ const App = () => {
     });
   }, []);
 
-
   return (
     <BrowserRouter>
       <Routes>
@@ -43,7 +43,7 @@ const App = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/app/*" element={<Main />} />
-        <Route path="/devs" element={<Dev />} />
+        {/* <Route path="/devs" element={<Dev />} /> */}
       </Routes>
     </BrowserRouter>
   );
